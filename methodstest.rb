66079a70271fd67e064ebe779eca83c0a192fb05
Jennifer Nicely <pipeline38@hotmail.com>
2019-08-27 17:49:54 +0000

    Automatically backed up by Learn

--- methodstest.rb
+++ methodstest.rb
@@ -3,20 +3,4 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-
-name = "Jen"
-language = "Ruby"
-def introduction_with_language(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-
-
-name = "Jen"
-language = "Java"
-def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-
 introduction
-introduction_with_language
-introduction_with_language_optional 
